4759a1f9f2f3f86a6da03150b46408dea854ab1c
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-10-18 18:49:08 -0300

    :sparkles: feat(novas-aulas): inclusão da aula 16

---
+++ src/aula16_strutural_type/aula16_strutural_type.ts
@@ -0,0 +1,39 @@
+type verifyUserFn = (user:User, sentUser:User ) => boolean;
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+}
+
+type Admin = {
+  id: number;
+  name: string;
+  email: string;
+}
+
+
+const verifyUser: verifyUserFn = (user:User, sentUser:User) => {
+  return user.id === sentUser.id && user.email === sentUser.email;
+}
+
+const user:User = {
+  id: 1,
+  name: "Fabricio",
+  email: "111",
+}
+
+const admin:Admin = {
+  id: 1,
+  name: "Leprechaun",
+  email: "222"
+}
+
+const userSent ={
+  id: 1,
+  name: "Fabricio",
+  email: "111",
+  permissions: ["read", "write"]
+}
+
+console.log(verifyUser(admin,userSent))
