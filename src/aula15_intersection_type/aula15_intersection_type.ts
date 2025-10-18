0417d86d71405150ece916a0d7314dc8b8c745f2
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-10-18 18:05:27 -0300

    :sparkles: feat(novas-aulas): inclusão da aula 15

---
+++ src/aula15_intersection_type/aula15_intersection_type.ts
@@ -0,0 +1,13 @@
+
+type NameCustomer = {name: string};
+type AgeCustomer = {age: number};
+type LastNameCustomer =  {lastName: string};
+type Customer = NameCustomer & AgeCustomer & LastNameCustomer;
+
+const customer:Customer = {
+  name: "Fabricio",
+  age: 30,
+  lastName: "Silva"
+};
+
+console.log(customer);
