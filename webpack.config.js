51d2acb5fba193eb071fefe3c5eecd3082bafb03
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-10-20 22:56:42 -0300

    :sparkles: feat(novas-aulas): inclusão de arquivos html e css para exercicio pratico

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode:   'development',
-  entry: './src/aula18_webpack/index.ts',
+  entry: './src/aula19_exercicio/index.ts',
   module: {
     rules: [
       {
@@ -25,3 +25,4 @@ module.exports = {
   },
   devtool: 'source-map',
 };
+ 
