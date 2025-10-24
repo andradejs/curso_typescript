b06e2144365cb233a353a19e7f1ba452f132e699
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-10-23 22:38:20 -0300

    :sparkles: feat(aulas): inclusão da aula 21 que remete a criação de classes com typescript

---
+++ src/aula21_class_in_typescript/class.ts
@@ -0,0 +1,52 @@
+export class Colaborador {
+
+  constructor(
+    public readonly nome: string,
+    public readonly sobrenome: string
+  ) {}
+
+  public getNome():string{
+    return this.nome;
+  }
+
+  public getSobrenome():string{
+    return this.sobrenome;
+  }
+
+}
+
+
+export class Empresa {
+
+  public readonly nome: string;
+  private readonly colaboradores: Colaborador[] = [];
+  protected readonly cnpj: string;
+
+  constructor(nome: string, cnpj: string) {
+    this.nome = nome;
+    this.cnpj = cnpj;
+  }
+
+  public addColaborador(...colaboradores:Colaborador[]):void {
+
+    colaboradores.forEach((colaborador)=>{
+      this.colaboradores.push(colaborador);
+
+    })
+  }
+
+  showColaboradores(){
+    this.colaboradores.forEach((colaborador)=>{
+      console.log("colaborador:",  colaborador.nome + " " + colaborador.sobrenome);
+    })
+  }
+}
+
+const empresa1 = new Empresa("paraiba", "aosijoajsooasjjo");
+const colaborador1 = new Colaborador("Pablo","Marsal");
+const colaborador2 = new Colaborador("Neymar","JR");
+const colaborador3 = new Colaborador("Ronaldinho", "Gaucho");
+
+empresa1.addColaborador(colaborador1,colaborador2,colaborador3);
+// console.log(empresa1)
+empresa1.showColaboradores();
