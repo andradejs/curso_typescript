a4dad026cbb9f17bac89c9dac2397213fa4e21eb
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-10-25 23:38:42 -0300

    :test: feat(aulas): inclusão do codigo da aula 23 com conseito de heranca

---
+++ src/aula23_inheritance_typescript/inheritance.ts
@@ -0,0 +1,48 @@
+export class Person {
+
+  constructor(
+    public name: string,
+    public lastName: string,
+    private cpf: string
+  ) { }
+
+  getFullName(): string {
+    return this.name + " " + this.lastName;
+  }
+
+  getLastName(): string {
+    return this.lastName;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getCPF(): string {
+    return this.cpf;
+  }
+}
+
+
+// Cliente herda todos os elementos da class Person
+export class Custumer extends Person {
+
+  getName() {
+    return "nome do cliente" + " " + this.name;
+  }
+}
+
+export class Employee extends Person {
+
+  getName(): string {
+    return "nome do funcionario" + " " + this.name;
+  }
+}
+
+const custumer = new Custumer("Leone", "Rodriges", "000.000.000-00");
+const employee = new Employee("Leone", "Rodriges", "000.000.000-00");
+const person = new Person("Leone", "Rodriges", "000.000.000-00");
+
+custumer.getName();
+employee.getName();
+person.getName();
