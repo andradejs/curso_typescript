91cdca96ceb682dd6da3642f0d0ac45b837d568a
Fabricio Andrade <caped.2022123tads0039@aluno.ifpi.edu.br>
2025-12-01 14:34:36 -0300

    :sparkles: feat(aulas): inclusão da aula 33 que mostra como criar tipos em typescript

---
+++ src/aula33_types_alias/index.ts
@@ -0,0 +1,21 @@
+type TipoPessoa = {
+  nome: string;
+  sobrenome: string;
+  nomeCompleto(): string;
+}
+
+export class Pessoa implements TipoPessoa {
+
+  constructor(
+    public nome: string,
+    public sobrenome: string
+  ) { }
+
+  nomeCompleto(): string {
+    return this.nome + " " + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa("Fabricio", "Silva");
+
+console.log(pessoa.nomeCompleto());
